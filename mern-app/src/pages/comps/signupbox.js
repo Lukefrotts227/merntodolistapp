0861640eb6f210926e5f1f2e29fc2c7fab22ca8c
Ljf227 <ljfrotton@gmail.com>
2023-06-03 18:41:17 -0400

    continue the signup box

--- mern-app/src/pages/comps/signupbox.js
+++ mern-app/src/pages/comps/signupbox.js
@@ -1,3 +1,16 @@
 import { createUser } from "../../api";
 import React, {useState} from 'react'; 
 
+function SignUpBox(){
+
+
+    return(
+        <div>
+
+        </div>
+    ); 
+}
+
+
+export default SignUpBox; 
+
