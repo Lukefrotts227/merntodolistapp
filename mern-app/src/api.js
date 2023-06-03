2d4a9b217cfb4c07da6ae60c0e327a1134fc3af7
Ljf227 <ljfrotton@gmail.com>
2023-06-03 18:36:13 -0400

    Starting the axios api

--- mern-app/src/api.js
+++ mern-app/src/api.js
@@ -1,1 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
+
+const URI_API = 'https:localhost:8082';
+
+
+export const createUser = async(user)=>{
+    const response = await axios.post (`${URI_API}/users/createUser`, user);
+    return response;
+}
