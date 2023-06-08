011229484e47f6126731e96199c38cfda2908159
Ljf227 <ljfrotton@gmail.com>
2023-06-08 10:48:51 -0400

    connecting the db file to the main server file

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const app = express();
 const UserModel = require('./models/User'); 
 
 const cors = require('cors'); 
+const connectDB = require('./config/db'); 
+
+app.use(express.json()); 
+app.use(cors()); 
+
+const port = 5000; 
+
+
+
+connectDB(); 
