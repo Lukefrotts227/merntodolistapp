1a3e4ba9a6491f9d6afcb4a8fc033d0c00a33128
Ljf227 <ljfrotton@gmail.com>
2023-06-13 09:03:43 -0400

    next step is to add the the frontend signup logic

---
+++ my-mern/src/pages/comps/signupbox.js
@@ -0,0 +1,9 @@
+
+import './styles/style1.css';
+
+function SignUpBox(){
+
+}
+
+
+export default SignUpBox; 
---
+++ my-mern/src/pages/signuppage.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+import './styles/style1.css'; 
+
+
+function SignUp(){
+    
+}
+
+export default SignUp; 
