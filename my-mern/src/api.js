60642139c95c9e014e11a1a66102d93f15f60c55
Ljf227 <ljfrotton@gmail.com>
2023-06-13 08:56:24 -0400

    User creation frontend api

--- my-mern/src/api.js
+++ my-mern/src/api.js
@@ -1,3 +1,8 @@
 import axios from 'axios'; 
 
 const URL = 'http://localhost:5000'; 
+
+export const createUser = async(user)=>{
+    const response = await axios.post('${URL}/users/createUser', user); 
+    return response; 
+}
